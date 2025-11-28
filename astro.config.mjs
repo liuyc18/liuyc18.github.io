425232f14f62ec0878dbf3e0e87e3ea70f7c8fd4
Liu Yichao <yichao.liu@u.nus.edu>
2025-11-28 14:21:00 +0800

    feat: add blog and projects pages with math/pdf support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeMathjax from 'rehype-mathjax/svg';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://liuyc18.github.io',
+  integrations: [mdx({
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathjax],
+  })],
+  markdown: {
+    // These apply to .md files
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathjax],
+  },
 });
